de79fafcdae83caa91fe9490fc4999796a132e2c
ShayanSolid <Shayan.shsh@yahoo.com>
2019-05-26 17:21:20 +0200

    DH-31 added Unit tests for DisturbanceTypeControllerService

--- src/main/java/com/software/technology/ss2019/dormheroes/service/DisturbanceTypeControllerService.java
+++ src/main/java/com/software/technology/ss2019/dormheroes/service/DisturbanceTypeControllerService.java
@@ -2,6 +2,7 @@ package com.software.technology.ss2019.dormheroes.service;
 
 import com.software.technology.ss2019.dormheroes.model.DisturbanceType;
 import com.software.technology.ss2019.dormheroes.repositories.DisturbanceTypeRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -18,5 +19,9 @@ public class DisturbanceTypeControllerService {
 
     public DisturbanceType createDisturbanceType(DisturbanceType disturbanceType){
         return disturbanceTypeRepository.insert(disturbanceType);
+    }
+
+    public void deleteDisturbanceTypeById(ObjectId id){
+        disturbanceTypeRepository.deleteById(id.toString());
     }
 }
---
+++ src/test/java/com/software/technology/ss2019/dormheroes/DisturbanceTypeControllerServiceTest.java
@@ -0,0 +1,72 @@
+package com.software.technology.ss2019.dormheroes;
+
+import com.software.technology.ss2019.dormheroes.controller.DisturbanceTypeController;
+import com.software.technology.ss2019.dormheroes.model.DisturbanceType;
+import com.software.technology.ss2019.dormheroes.repositories.DisturbanceTypeRepository;
+import com.software.technology.ss2019.dormheroes.service.DisturbanceTypeControllerService;
+import org.bson.types.ObjectId;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DisturbanceTypeControllerServiceTest {
+
+    @Autowired
+    private DisturbanceTypeControllerService disturbanceTypeControllerService;
+
+
+    @Test
+    public void listOfAllDisturbanceTypesShouldNotBeEmpty(){
+        ObjectId testId = new ObjectId();
+        DisturbanceType testDisturbanceType = new DisturbanceType(testId,
+                "created for SpringDisturbanceType"
+        );
+        disturbanceTypeControllerService.createDisturbanceType(testDisturbanceType);
+        List<DisturbanceType> disturbanceTypeList = disturbanceTypeControllerService.getAllDisturbanceType();
+        Assert.assertFalse("List of all DisturbanceTypes should not be empty" , disturbanceTypeList.isEmpty());
+        disturbanceTypeControllerService.deleteDisturbanceTypeById(testId);
+    }
+
+    @Test
+    public void deletedDisturbanceTypeShouldNotExistInDB(){
+        ObjectId testId = new ObjectId();
+        DisturbanceType testDisturbanceType = new DisturbanceType(testId,
+                "created for SpringDisturbanceType"
+        );
+        disturbanceTypeControllerService.createDisturbanceType(testDisturbanceType);
+        disturbanceTypeControllerService.deleteDisturbanceTypeById(testId);
+        List<DisturbanceType> disturbanceTypeList = disturbanceTypeControllerService.getAllDisturbanceType();
+
+        List<DisturbanceType> deletedDisturbanceType = disturbanceTypeList.stream()
+                .filter(disturbanceType -> disturbanceType.get_id().equals(testId.toString()))
+                .collect(Collectors.toList());
+
+        Assert.assertTrue("The removed DisturbanceType should not exist in DB" , deletedDisturbanceType.isEmpty());
+    }
+
+    @Test
+    public void getDisturbanceTypeByIdShouldReturnTheCorrectDisturbanceType(){
+        ObjectId testId = new ObjectId();
+        DisturbanceType testDisturbanceType = new DisturbanceType(testId,
+                "created for SpringDisturbanceType"
+        );
+        disturbanceTypeControllerService.createDisturbanceType(testDisturbanceType);
+        List<DisturbanceType> disturbanceTypeList = disturbanceTypeControllerService.getAllDisturbanceType();
+
+        List<DisturbanceType> createdDisturbanceType = disturbanceTypeList.stream()
+                .filter(disturbanceType -> disturbanceType.get_id().equals(testId.toString()))
+                .collect(Collectors.toList());
+
+        Assert.assertFalse("The searched DisturbanceType should be an exact match" , createdDisturbanceType.isEmpty());
+        disturbanceTypeControllerService.deleteDisturbanceTypeById(testId);
+    }
+
+}
