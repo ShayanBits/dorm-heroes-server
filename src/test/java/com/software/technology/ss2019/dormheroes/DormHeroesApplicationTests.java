f58eb1ec32d11b87cabec9c886dd6051f260673a
Anton Ponomarenko <anton.ponomarenko@gmx.de>
2019-06-14 18:37:58 +0200

    DH-40: access the correct database from the propety files and use test profile for unit tests

--- src/test/java/com/software/technology/ss2019/dormheroes/DormHeroesApplicationTests.java
+++ src/test/java/com/software/technology/ss2019/dormheroes/DormHeroesApplicationTests.java
@@ -3,9 +3,11 @@ package com.software.technology.ss2019.dormheroes;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles(profiles = "test")
 @SpringBootTest
 public class DormHeroesApplicationTests {
 
